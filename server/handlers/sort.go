7291a5a3087d87f1fb1a4af37003133a8997dbf1
Salsasp <dakotagee99@gmail.com>
2026-03-22 14:41:58 -0500

    adjust random number data

--- server/handlers/sort.go
+++ server/handlers/sort.go
@@ -6,15 +6,16 @@ import (
 	"net/http"
 )
 
-const NUMBER_ARRAY_SIZE = 100
+const NUMBER_ARRAY_SIZE = 200
 
 func GetNumbers(w http.ResponseWriter, r *http.Request) {
 	nums := make([]int, NUMBER_ARRAY_SIZE)
 
 	for i := range nums {
-		nums[i] = rand.IntN(100)
+		nums[i] = rand.IntN(500)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*") //TODO: lock this down in prod
 	json.NewEncoder(w).Encode(nums)
 }
